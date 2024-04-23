66678f525917ed666bbc8afd9f36af38dac4899f
nulluser <diuyeung@gmail.com>
2024-04-23 20:08:30 +1000

    workspace tab

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-const { invoke } = window.__TAURI__.tauri;
+// const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+// Main Tab System
+$(document).ready(function(){
+  $('#tab-links #tab-link').on('click', function(){
+    var targetTab = $(this).data('tab');
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+    // Show/Hide Tabs
+    $(targetTab).show().siblings('#tabs-contents div').hide();
+
+    // Change/remove current tab to active
+    $(this).addClass('active').siblings('#tab-link').removeClass('active');
   });
 });
