66a8b61aa2035dd119698ffaf2113943d9af292d
null <151502370+nulluser0@users.noreply.github.com>
2024-05-01 16:05:11 +1000

    better tab control

--- src/main.js
+++ src/main.js
@@ -1,29 +1,59 @@
 const { invoke } = window.__TAURI__.tauri;
 
-import { register } from '@tauri-apps/api/globalShortcut';
+const { register } = window.__TAURI__.globalShortcut;
 
 function getLineNumber(textarea, indicator) {
   indicator.innerHTML = textarea.value.substr(0, textarea.selectionStart).split("\n").length;
 }
 
-// Main Tab System
-$(document).ready(function(){
-  $('#tabLinks #tabLink').on('click', function(){
-    var targetTab = $(this).data('tab');
+// New tab. (Ctrl + T)
+function newTab() {
+  var newTabId = $('#tabLinks #tabLink').length + 1;
+  var newTab = $('<li id="tabLink" data-tab="#tab' + newTabId + '">Tab ' + newTabId + '</li>');
+  var newTabContent = $('<div id="tab' + newTabId + '"><textarea class="text-input"></textarea></div>')
+  $('#tabNewTabButton').before(newTab);
+  $('#mainContainer').children().append(newTabContent);
 
-    // Show/Hide Tabs
-    $(targetTab).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
+  $(newTab).addClass('active').siblings('#tabLink').removeClass('active');
+  $(newTabContent).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
+}
 
-    // Change/remove current tab to active
-    $(this).addClass('active').siblings('#tabLink').removeClass('active');
-  });
+// Tab after. (Ctrl + Tab)
+function nextTab() {
+  var currentActiveTab = $('#tabLinks .active');
+  var nextTab = currentActiveTab.next('#tabLink');
 
-  $('#tabNewTabButton').on('click', function newTab(){
-    var newTab = $('<li id="tabLink"></li>').text('New Tab').data('tab', 10);
-    $('#tabNewTabButton').before(newTab);
-  })
+  if (nextTab.length === 0) {
+    // If there is no next tab, switch to the first tab
+    console.log("No next tab. Goto First.")
+    nextTab = $('#tabLinks #tabLink').first();
+  }
+
+  var targetTab = $(nextTab).data('tab');
+
+  // Show/Hide Tabs
+  $(targetTab).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
+
+  // Change/remove current tab to active
+  $(nextTab).addClass('active').siblings('#tabLink').removeClass('active');
+}
+
+$(document).on('click', '#tabLink', function(){
+  var targetTab = $(this).data('tab');
+
+  // Show/Hide Tabs
+  $(targetTab).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
+
+  // Change/remove current tab to active
+  $(this).addClass('active').siblings('#tabLink').removeClass('active');
 });
+  
+$('#tabNewTabButton').on('click', newTab);
 
 await register('CommandOrControl+T', () => {
   newTab();
 });
+
+await register('CommandOrControl+Tab', () => {
+  nextTab();
+})
