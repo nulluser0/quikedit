c2c5e78195934fc473468526041b55b6d3da83cd
nulluser <diuyeung@gmail.com>
2024-05-01 23:48:02 +1000

    added home screen. Also added better tab support.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ function getLineNumber(textarea, indicator) {
   indicator.innerHTML = textarea.value.substr(0, textarea.selectionStart).split("\n").length;
 }
 
+function showDefaultPageCheck() {
+  if ($('#tabLinks #tabLink').length === 0) {
+    $('#defaultPage').removeClass('hidden');
+  }
+}
+
 // New tab. (Ctrl + T)
 function newTab() {
   var newTabId = $('#tabLinks #tabLink').length + 1;
@@ -18,6 +24,37 @@ function newTab() {
   $(newTabContent).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
 }
 
+function deleteTab() {
+  var currentDeletionTab = $('#tabLinks .active');
+  var previousTab = currentDeletionTab.prev('#tabLink');
+  if (previousTab.length === 0) {
+    // If there is no previous tab, next tab
+    previousTab = currentDeletionTab.next('#tabLink');
+  }
+  if (previousTab.length === 0) {
+    previousTab = null;
+  }
+
+  var targetDeletionTab = $(currentDeletionTab).data('tab');
+  var targetNextTab = $(previousTab).data('tab');
+
+  // TODO: There should be an area which stops/warns you before deleting the current tab.
+
+  // Show/Hide Tabs
+  $(targetDeletionTab).addClass('hidden');
+  $(targetNextTab).removeClass('hidden');
+
+  // Change/remove current tab to active
+  $(currentDeletionTab).removeClass('active');
+  $(previousTab).addClass('active');
+
+  // Delete tabs.
+  $(currentDeletionTab).remove();
+  $(targetDeletionTab).remove();
+
+  showDefaultPageCheck()
+}
+
 // Tab after. (Ctrl + Tab)
 function nextTab() {
   var currentActiveTab = $('#tabLinks .active');
@@ -25,8 +62,26 @@ function nextTab() {
 
   if (nextTab.length === 0) {
     // If there is no next tab, switch to the first tab
-    console.log("No next tab. Goto First.")
     nextTab = $('#tabLinks #tabLink').first();
+  }
+
+  var targetTab = $(nextTab).data('tab');
+
+  // Show/Hide Tabs
+  $(targetTab).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
+
+  // Change/remove current tab to active
+  $(nextTab).addClass('active').siblings('#tabLink').removeClass('active');
+}
+
+// Tab before. (Ctrl + Shift + Tab)
+function prevTab() {
+  var currentActiveTab = $('#tabLinks .active');
+  var nextTab = currentActiveTab.prev('#tabLink');
+
+  if (nextTab.length === 0) {
+    // If there is no next tab, switch to the first tab
+    nextTab = $('#tabLinks #tabLink').last();
   }
 
   var targetTab = $(nextTab).data('tab');
@@ -56,4 +111,12 @@ await register('CommandOrControl+T', () => {
 
 await register('CommandOrControl+Tab', () => {
   nextTab();
+})
+
+await register('CommandOrControl+Shift+Tab', () => {
+  prevTab();
+})
+
+await register('CommandOrControl+W', () => {
+  deleteTab();
 })
