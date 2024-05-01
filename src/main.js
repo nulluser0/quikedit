968e0be606f4e981b25498b925d2710742c464dc
null <151502370+nulluser0@users.noreply.github.com>
2024-05-01 12:34:28 +1000

    tab system improvements

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-// const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri;
 
+import { register } from '@tauri-apps/api/globalShortcut';
+
+function getLineNumber(textarea, indicator) {
+  indicator.innerHTML = textarea.value.substr(0, textarea.selectionStart).split("\n").length;
+}
 
 // Main Tab System
 $(document).ready(function(){
@@ -7,9 +12,18 @@ $(document).ready(function(){
     var targetTab = $(this).data('tab');
 
     // Show/Hide Tabs
-    $(targetTab).removeAttr('hidden').siblings('#tabsContents div').attr('hidden', 'hidden');
+    $(targetTab).removeClass('hidden').siblings('#tabsContents div').addClass('hidden');
 
     // Change/remove current tab to active
     $(this).addClass('active').siblings('#tabLink').removeClass('active');
   });
+
+  $('#tabNewTabButton').on('click', function newTab(){
+    var newTab = $('<li id="tabLink"></li>').text('New Tab').data('tab', 10);
+    $('#tabNewTabButton').before(newTab);
+  })
+});
+
+await register('CommandOrControl+T', () => {
+  newTab();
 });
