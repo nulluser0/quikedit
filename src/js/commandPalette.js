866bab04c6412d44458c162d556f8a9c78d95338
nulluser <diuyeung@gmail.com>
2024-05-05 21:13:34 +1000

    seperation to multiple files

---
+++ src/js/commandPalette.js
@@ -0,0 +1,15 @@
+// Open
+export function openCommandPalette() {
+    $('#commandPalette').removeClass('hidden');
+    $('#commandPalette').find('input').trigger('focus');
+}
+
+// Close
+export function closeCommandPalette() {
+    $('#commandPalette').addClass('hidden');
+    $('#commandPalette').find('input').trigger('blur');
+
+    // let currentTab = $('#tabLinks .active');
+    // let currentTargetTab = $(currentTab).data('tab');
+    // $(currentTargetTab).children('.text-input').trigger("focus");
+}
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// const { invoke } = window.__TAURI__.tauri;
+
+jQuery.hotkeys.options.filterInputAcceptingElements = false
+
+import * as tabManagement from './tabManagement.js';
+import * as commandPalette from './commandPalette.js';
+import * as textInputManagement from './textInputManagement.js';
+
+import * as keyboardShortcuts from './keyboardShortcuts.js';
