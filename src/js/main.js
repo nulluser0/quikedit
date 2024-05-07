aae03932c385d4b16faf0c4820086f2433e7e136
nulluser <diuyeung@gmail.com>
2024-05-07 20:49:04 +1000

    made start on toast service

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,6 @@ import * as commandPalette from './commandPalette.js';
 import * as textInputManagement from './textInputManagement.js';
 import * as fileUtils from './fileUtils.js';
 import * as gateway from './gateway.js';
+import * as toast from './toast.js';
 
 import * as keyboardShortcuts from './keyboardShortcuts.js';
